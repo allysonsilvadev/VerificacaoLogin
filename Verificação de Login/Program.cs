ec3cd45017d5f8a6065c774ec1ce4980100ad924
allysonsilvadev <allysonsilvap733@gmail.com>
2025-11-04 13:52:34 -0300

    Adicionar arquivos de projeto.

---
+++ Verificação de Login/Program.cs
@@ -0,0 +1,22 @@
+﻿//Verificação de Login. Peça usuário e senha e compare com valores pré-definidos.
+//Exemplo: usuário = "admin", senha = "1234".
+//Se certo → "Acesso permitido", caso contrário → "Acesso negado"
+
+String usuario = "admin";
+int senha = 1234;
+
+Console.WriteLine("Digite o nome de usuário: ");
+usuario = Console.ReadLine();
+
+Console.WriteLine("Digite a senha: ");
+senha = int.Parse(Console.ReadLine());
+
+if  (usuario == "admin" &&  senha == 1234)
+{
+    Console.WriteLine("Acesso permitido");
+}
+else
+{
+    Console.WriteLine("Acesso negado");
+    return;
+}
